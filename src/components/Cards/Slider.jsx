f45cad8722ab0ef0214a1d76cee7512e910bd1ad
Omar Alomar <omaro2000@gmail.com>
2022-01-07 21:58:42 -0500

    made slider fully responsive

--- src/components/Cards/Slider.jsx
+++ src/components/Cards/Slider.jsx
@@ -13,15 +13,32 @@ function Slider() {
         <>
         <div className="swipertest">
                 <Swiper
-                slidesPerView={3}
+                slidesPerView={1}
                 spaceBetween={30}
-                slidesPerGroup={3}
+                slidesPerGroup={1}
                 loop={true}
                 loopFillGroupWithBlank={true}
                 pagination={{
                 clickable: true
                 }}
                 navigation={true}
+                breakpoints={{
+                    480: {
+                        slidesPerView: 2,
+                        spaceBetween: 20,
+                        slidesPerGroup: 2
+                      },
+                    768: {
+                      slidesPerView: 3,
+                      spaceBetween: 30,
+                      slidesPerGroup: 3
+                    },
+                    1024: {
+                        slidesPerView: 4,
+                        spaceBetween: 40,
+                        slidesPerGroup: 4
+                    }
+                  }}
                 className="mySwiper">
                 <SwiperSlide>
                     <CardItem src="images/mba.png" 
