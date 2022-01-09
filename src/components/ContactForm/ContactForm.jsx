7d3468649db3932821c0bbb040d5990d09157551
Omar Alomar <omaro2000@gmail.com>
2022-01-09 18:09:22 -0500

    finished contact form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,22 +1,52 @@
 import React from 'react'
 import './ContactForm.css'
 
+// Source: https://codepen.io/JonLehman/pen/yOdbOG
+
 function ContactForm() {
     return (
         <>
             <div className="contact_container">
-                <h2>Fill out the form below to contact us directly:</h2>
-                <form action="https://formsubmit.co/b9498839c8d5904e7cf3f250dc8f3ca2" method="POST">
-                    <text>Name:</text>
-                    <input type="name" name="name" required/>
-                    <text>Email:</text>
-                    <input type="email" name="email" required/>
-                    <text>Message:</text>                        
-                    <input type="text" name="text" required/> 
-                    {/* TODO: thank you page redirect
-                        <input type="hidden" name="_next" value="localhost:3000"></input>*/}
-                    <button type="submit" name="submit">Submit</button>                       
-                </form> 
+                 <form action="https://formsubmit.co/omaro2000@gmail.com" method="POST">
+                    <div class="container">
+                        <div class="row">
+                                <h1>Contact Us</h1>
+                        </div>
+                        <div class="row">
+                                <h4>We'd love to hear from you!</h4>
+                        </div>
+                        <div class="row input-container">
+                                <div class="col-xs-12">
+                                    <div class="styled-input wide">
+                                        <input type="text" name="fname" required />
+                                        <label>Name</label> 
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-sm-12">
+                                    <div class="styled-input">
+                                        <input type="email" name="email"required />
+                                        <label>Email</label> 
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-sm-12">
+                                    <div class="styled-input" style={{float:"right"}}>
+                                        <input type="tel" name="tel" required />
+                                        <label>Phone Number</label> 
+                                    </div>
+                                </div>
+                                <div class="col-xs-12">
+                                    <div class="styled-input wide">
+                                        <textarea name="msg" required></textarea>
+                                        <label>Message</label>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12">
+                                    <button class="submit">Send Message</button>
+                                </div>
+                        </div>
+                    </div>
+
+                </form>
             </div>
         </>
     )
--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -5,7 +5,6 @@ import ContactForm from '../ContactForm/ContactForm'
 export default function Contact() {
     return (
         <>
-            <h1 className="contact">Contact</h1>
             <ContactForm/>
         </>
     )
